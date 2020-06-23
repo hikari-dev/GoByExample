8d04959074febc975108255fd2fb3fe3e909faf1
hikari <zhiyoushangdicainengshenpanwo@gmail.com>
2020-06-23 16:59:39 +0800

    Initial commit

---
+++ 32.timers/timers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	timer1 := time.NewTimer(2 * time.Second)
+	fmt.Println("Timer 1 created")
+	<-timer1.C
+	fmt.Println("Timer 1 fired")
+
+	timer2 := time.NewTimer(time.Second)
+	fmt.Println("Timer 2 created")
+	go func() {
+		<-timer2.C
+		fmt.Println("Timer 2 fired")
+	}()
+	stop2 := timer2.Stop()
+	if stop2 {
+		fmt.Println("Timer 2 Stopped")
+	}
+
+	time.Sleep(2 * time.Second)
+}
